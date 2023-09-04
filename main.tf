936d34c0d6702f5081a662a038d915379da577e1
abdimalik2010 <abdimalik08@icloud.com>
2023-09-04 22:20:09 +0200

    CD added

--- main.tf
+++ main.tf
@@ -65,6 +65,7 @@ module "acr" {
   sku                 = "Standard"
   admin_enabled       = false
 }
+
 module "acr_aks_assignment" {
   source           = "./modules/acr-aks-assignment"
   acr_id           = module.acr.acr_id
