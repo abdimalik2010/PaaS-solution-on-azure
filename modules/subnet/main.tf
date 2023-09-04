47d0d6fc4539c652824b500de92c5b871cbfdf22
abdimalik2010 <abdimalik08@icloud.com>
2023-09-04 19:15:51 +0200

    mit

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -4,4 +4,5 @@ resource "azurerm_subnet" "aks" {
   address_prefixes     = var.address_prefixes
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
+  
 }
